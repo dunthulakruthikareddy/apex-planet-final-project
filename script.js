c5a5bb1226658a96ec6a3cd196c0e5b3ba693f83
kruthika reddy <kruthikadunthula@gmail.com>
2026-07-03 18:28:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function shopNow() {
+    alert("Welcome to ShopMart! Start Shopping.");
+}
+
+let cartCount = 0;
+
+function addCart() {
+    cartCount++;
+    alert("Product added to cart!\nTotal Items: " + cartCount);
+}
